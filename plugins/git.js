5472bd5bb6c53e2ba33565fc9e726ead1b6f76d1
Monuzz-Mkz <92070795+Monuzz-Mkz@users.noreply.github.com>
2021-10-23 05:05:50 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,28 +1,15 @@
-const Asena = require('../events');
-const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
-const axios = require('axios');
-
-const Language = require('../language');
-const Lang = Language.getString('wallpaper');
-
-Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
-
-    var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
-
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Ameer Suhail*
-*Creator number : wa.me/918848120533?text=Hi%20Ameer%20Suhail.%20*
-
-*Bot setting video : https://youtu.be/7Q01xo8jJw4*
-
-*Githublink (Setup)  :    https://github.com/Monuzz-Mkz/aswathyy*
-
-*Audio commads :   https://github.com/Monuzz-Mkz/aswathyy/tree/master/uploads*
-
-*Sticker commads : https://github.com/Monuzz-Mkz/aswathyy/tree/master/uploads*
-
-*Video For Setting Bot : https://youtu.be/7Q01xo8jJw4* 
-
-*For More Updates Subscribe The Channel ☝*
+const Asena=require('\x2E\x2E\x2F\x65\x76\x65\x6E\x74\x73');//0
+const {MessageType,MessageOptions,Mimetype}=require('\x40\x61\x64\x69\x77\x61\x6A\x73\x68\x69\x6E\x67\x2F\x62\x61\x69\x6C\x65\x79\x73');//1
+const axios=require('\x61\x78\x69\x6F\x73');//2
+const Language=require('\x2E\x2E\x2F\x6C\x61\x6E\x67\x75\x61\x67\x65');//4
+const Lang=Language.getString('\x67\x69\x74\x6C\x69\x6E\x6B');//5
+Asena.addCommand({pattern:'\x67\x69\x74',fromMe:false,desc:Lang.GL},(async (_0x171D5,_0x17177)=>
+{
+	var _0x17233= new Array();//9
+	_0x17233[0]= "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x61\x76\x61\x74\x61\x72\x73\x2E\x67\x69\x74\x68\x75\x62\x75\x73\x65\x72\x63\x6F\x6E\x74\x65\x6E\x74\x2E\x63\x6F\x6D\x2F\x75\x2F\x38\x35\x36\x36\x34\x39\x33\x36\x3F\x76\x3D\x34";var _0x17119=Math.floor(1* Math.random());//16
+	var _0x17291= await axios["\x67\x65\x74"](`${_0x17233[_0x17119]}`,{responseType:'\x61\x72\x72\x61\x79\x62\x75\x66\x66\x65\x72'});//18
+	 await _0x171D5.sendMessage(Buffer(_0x17291.data),MessageType.image,{mimetype:Mimetype.png,caption:`*ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴋᴀʀᴛʜɪᴋ-ᴛᴇʀʀᴏʀ-ʙᴏʏ*
+*🎀𝗔𝘀𝘄𝗮𝘁𝗵𝘆 𝗯𝗼𝘁🎀* \n\n *𝐁𝐨𝐭 𝐂𝐫𝐞𝐚𝐭𝐢𝐧𝐠 𝐕𝐞𝐝𝐢𝐨* \n\n *☞︎︎︎https://youtu.be/7Q01xo8jJw4* \n\n *𝐆𝐈𝐓 𝐇𝐔𝐁* \n\n *☞︎︎︎https://github.com/Monuzz-Mkz/aswathyy* \n\n *𝙋𝙪𝙗𝙡𝙞𝙘 𝘽𝙤𝙩 𝙂𝙧𝙤𝙪𝙥* \n\n *☞︎︎︎https://chat.whatsapp.com/CgWt3VG4FMtAhPPJiRAivW* \n\n *🎀𝗔𝘀𝘄𝗮𝘁𝗵𝘆 𝗯𝗼𝘁 𝗦𝘂𝗽𝗽𝗼𝗿𝘁🎀* \n\n *𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗚𝗿𝗼𝘂𝗽 𝗹𝗶𝗻𝗸* \n\n *☞︎︎︎https://chat.whatsapp.com/Ktg2pOcryOD8T7FmKjauXy* \n\n *𝘼𝙙𝙢𝙞𝙣* \n\n *☞︎︎︎https://wa.me/918848120533
 `}) 
 
 }));
