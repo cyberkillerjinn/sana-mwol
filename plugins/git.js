9264263ecccf27e49b6d019f24b7c31a76543f3a
Monuzz-Mkz <92070795+Monuzz-Mkz@users.noreply.github.com>
2021-10-23 00:21:56 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -10,17 +10,17 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
     var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
+*Creator number : wa.me/918848120533?text=Hi%20Ameer%20Suhail.%20*
 
-*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
+*Bot setting video : https://youtu.be/7Q01xo8jJw4*
 
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
+*Githublink (Setup)  :    https://github.com/Monuzz-Mkz/aswathyy*
 
-*Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+*Audio commads :   https://github.com/Monuzz-Mkz/aswathyy/tree/master/uploads*
 
-*Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+*Sticker commads : https://github.com/Monuzz-Mkz/aswathyy/tree/master/uploads*
 
-*Video For Setting Bot : https://youtu.be/_D4ZYuUSXjs* 
+*Video For Setting Bot : https://youtu.be/7Q01xo8jJw4* 
 
 *For More Updates Subscribe The Channel ☝*
 `}) 
